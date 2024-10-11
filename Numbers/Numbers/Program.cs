43e82bf48d1f9f07b5c09003ed3a483c08338978
ekattrina <141343@student.uni.opole.pl>
2024-10-11 22:31:13 +0300

    Removed AssemblyInfo.cs from tracking and updated .gitignore

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Give a number");
+        bool isNumber = double.TryParse(Console.ReadLine(), out var number);
+
+        if (!isNumber)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+        else
+        {
+            switch (number)
+            {
+                case double n when (n >= 0 && n <= 14):
+                    Console.WriteLine("The number belongs to [0;14]");
+                    break;
+                case double n when (n >= 15 && n <= 35):
+                    Console.WriteLine("The number belongs to [15;35]");
+                    break;
+                case double n when (n >= 36 && n <= 50):
+                    Console.WriteLine("The number belongs to [36;50]");
+                    break;
+                case double n when (n >= 51 && n <= 100):
+                    Console.WriteLine("The number belongs to [51;100]");
+                    break;
+                default:
+                    Console.WriteLine("The number is not within any of the intervals");
+                    break;
+            }
+        }
+    }
+}
