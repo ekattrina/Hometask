db394109583315b914945d8da908e5ebfbb8e9f4
ekattrina <141343@student.uni.opole.pl>
2024-10-11 23:01:45 +0300

    Ads dictionary

--- Dictionary/Dictionary/Program.cs
+++ Dictionary/Dictionary/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>
+        {
+            {"облачно", "Cloudy"},   
+            {"ветрено", "Windy"},
+            {"солнечно", "Sunny"},
+            {"дождливо", "Rainy"},
+            {"пасмурно", "Foggy"},
+            {"снежно", "Snowy"},
+            {"влажно", "Humid"},
+            {"морозно", "Freezing"},
+            {"жарко", "Hot"},
+            {"сухо", "Dry"}
+        };
+
+        Console.WriteLine("Введите слово:");
+        string input = Console.ReadLine().ToLower(); 
+
+        if (dictionary.ContainsKey(input))
+        {
+            Console.WriteLine(dictionary[input]);
+        }
+        else
+        {
+            Console.WriteLine("Словарь не содержит этого слова");
+        }
+    }
+}
