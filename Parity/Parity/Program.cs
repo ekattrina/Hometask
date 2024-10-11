9a6a0f556d8571dbe370055a7ec3e1fd790146c3
ekattrina <141343@student.uni.opole.pl>
2024-10-11 23:21:49 +0300

    Adds methods to check parity

--- Parity/Parity/Program.cs
+++ Parity/Parity/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+     static void FirstMethod(int number)
+    {
+        Console.WriteLine(number % 2 == 0 ? "Even" : "Odd");
+    }
+
+    static void SecondMethod(int number)
+    {
+        Console.WriteLine((number & 1) == 0 ? "Even" : "Odd");
+    }
+      
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        bool isNumber = int.TryParse(Console.ReadLine(), out var number);
+
+        if (isNumber)
+        {
+            FirstMethod(number);
+            SecondMethod(number);
+        }
+        else
+        {
+            Console.WriteLine("Wrong input!");
+        }
+    }
+}
